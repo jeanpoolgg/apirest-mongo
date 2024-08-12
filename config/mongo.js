c27725fef210585f593413ee9346148d88a9b8f9
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2024-08-11 22:16:58 -0500

    Proyecto base

---
+++ config/mongo.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const dbConnect = async () => {
+    try {
+        const DB_URI = await process.env.DB_URI;
+        await mongoose.connect(DB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("CONEXION ESTABLECIDA CON MONGO");
+    } catch (e) {
+        console.log(e + "ERROR DE CONEXION");
+    }
+};
+
+module.exports = dbConnect
