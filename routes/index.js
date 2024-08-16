be71fac5977bd2de1657f9da7b4a90185a8a1948
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2024-08-15 21:11:42 -0500

    Implementacion de controladores

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+const express = require("express");
+const fs = require("fs")
 const router = express.Router();
 
 const PATH_ROUTES = __dirname;
--- routes/tracks.js
+++ routes/tracks.js
@@ -1,9 +1,8 @@
 const express = require("express");
+const { getItems, createItem} = require("../controllers/tracks");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    const data = ["hola", "mundo"]
-    res.send({data})
-})
+router.get("/", getItems)
+router.post("/", createItem)
 
 module.exports = router
