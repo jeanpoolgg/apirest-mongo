1c3d9a16837799f4f7cbf0503758e894e36f9f18
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2024-08-17 11:17:54 -0500

    Middleware basico

--- routes/tracks.js
+++ routes/tracks.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { getItems, createItem} = require("../controllers/tracks");
 const { validatorCreateItem } = require("../validators/tracks");
+const customHeader = require("../middleware/customHeader")
 
 
 router.get("/", getItems)
-router.post("/", validatorCreateItem ,createItem)
+router.post("/", validatorCreateItem, customHeader, createItem)
 
 module.exports = router
